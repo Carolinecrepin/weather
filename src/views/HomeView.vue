49d48155a2a50d64c1fa83b33b5010eb778115c9
caroline <crepin.caroline@gmail.com>
2022-04-08 17:06:38 +0200

    home templating

---
+++ src/views/HomeView.vue
@@ -0,0 +1,266 @@
+<template>
+    <div class="home">
+        <!--sidebar-->
+        <div class="menu">
+            <img src="../assets/menu.png" alt="menu" class="menu"/>
+        </div>
+        <!--card dashboard meteo-->
+        <div class="card-dashboard">
+            <div class="left-card-dashboard">
+                <h2 class="celsius">5°</h2>
+                <h4 class="city">Arras</h4>
+                <p>1°/6° Ressenti 3°</p>
+                <p>Vendredi, 11:41</p>
+            </div>
+            <div class="right-card-dashboard">
+                <img src="../assets/rain-clouds.png" alt="weather-img" class="weather-img"/>
+            </div>
+        </div>
+        <!--card meteo par heure-->
+        <div class="card">
+            <div class="hours-meteo">
+                <p><b>12:00</b></p>
+                <img src="../assets/rain-icon.png" alt="weather-icon" class="weather-icon"/>
+                <p>5°</p>
+                <li>_</li>
+                <p><img src="../assets/droplet.png" alt="weather-icon" class="weather-icon"/> 92%</p>
+            </div>
+             <div class="hours-meteo">
+                <p><b>12:00</b></p>
+                <img src="../assets/rain-icon.png" alt="weather-icon" class="weather-icon"/>
+                <p>5°</p>
+                <li>_</li>
+                <p><img src="../assets/droplet.png" alt="weather-icon" class="weather-icon"/> 92%</p>
+            </div>
+             <div class="hours-meteo">
+                <p><b>12:00</b></p>
+                <img src="../assets/rain-icon.png" alt="weather-icon" class="weather-icon"/>
+                <p>5°</p>
+                <li>_</li>
+                <p><img src="../assets/droplet.png" alt="weather-icon" class="weather-icon"/> 92%</p>
+            </div>
+             <div class="hours-meteo">
+                <p><b>12:00</b></p>
+                <img src="../assets/rain-icon.png" alt="weather-icon" class="weather-icon"/>
+                <p>5°</p>
+                <li>_</li>
+                <p><img src="../assets/droplet.png" alt="weather-icon" class="weather-icon"/> 92%</p>
+            </div>
+             <div class="hours-meteo">
+                <p><b>12:00</b></p>
+                <img src="../assets/rain-icon.png" alt="weather-icon" class="weather-icon"/>
+                <p>5°</p>
+                <li>_</li>
+                <p><img src="../assets/droplet.png" alt="weather-icon" class="weather-icon"/> 92%</p>
+            </div>
+             <div class="hours-meteo">
+                <p><b>12:00</b></p>
+                <img src="../assets/rain-icon.png" alt="weather-icon" class="weather-icon"/>
+                <p>5°</p>
+                <li>_</li>
+                <p><img src="../assets/droplet.png" alt="weather-icon" class="weather-icon"/> 92%</p>
+            </div>
+             <div class="hours-meteo">
+                <p><b>12:00</b></p>
+                <img src="../assets/rain-icon.png" alt="weather-icon" class="weather-icon"/>
+                <p>5°</p>
+                <li>_</li>
+                <p><img src="../assets/droplet.png" alt="weather-icon" class="weather-icon"/> 92%</p>
+            </div>
+        </div>
+        <!-- card meteo jours -->
+        <div class="card-row">
+                <div class="day-weather">
+                    <p>Aujourd'hui</p>
+                    <p><img src="../assets/droplet.png" alt="weather-icon" class="weather-icon"/> 100%</p>
+                    <img src="../assets/rain-icon.png" alt="weather-icon" width="25" height="25" class="weather-icon"/>
+                    <img src="../assets/rain-icon.png" alt="weather-icon" width="25" height="25" class="weather-icon"/>
+                    <p>5°</p>
+                    <p>1°</p>
+                </div>
+                <div class="day-weather">
+                    <p>Aujourd'hui</p>
+                    <p><img src="../assets/droplet.png" alt="weather-icon" class="weather-icon"/> 100%</p>
+                    <img src="../assets/rain-icon.png" alt="weather-icon" width="25" height="25" class="weather-icon"/>
+                    <img src="../assets/rain-icon.png" alt="weather-icon" width="25" height="25" class="weather-icon"/>
+                    <p>5°</p>
+                    <p>1°</p>
+                </div>
+                <div class="day-weather">
+                    <p>Aujourd'hui</p>
+                    <p><img src="../assets/droplet.png" alt="weather-icon" class="weather-icon"/> 100%</p>
+                    <img src="../assets/rain-icon.png" alt="weather-icon" width="25" height="25" class="weather-icon"/>
+                    <img src="../assets/rain-icon.png" alt="weather-icon" width="25" height="25" class="weather-icon"/>
+                    <p>5°</p>
+                    <p>1°</p>
+                </div>
+                <div class="day-weather">
+                    <p>Aujourd'hui</p>
+                    <p><img src="../assets/droplet.png" alt="weather-icon" class="weather-icon"/> 100%</p>
+                    <img src="../assets/rain-icon.png" alt="weather-icon" width="25" height="25" class="weather-icon"/>
+                    <img src="../assets/rain-icon.png" alt="weather-icon" width="25" height="25" class="weather-icon"/>
+                    <p>5°</p>
+                    <p>1°</p>
+                </div>
+                <div class="day-weather">
+                    <p>Aujourd'hui</p>
+                    <p><img src="../assets/droplet.png" alt="weather-icon" class="weather-icon"/> 100%</p>
+                    <img src="../assets/rain-icon.png" alt="weather-icon" width="25" height="25" class="weather-icon"/>
+                    <img src="../assets/rain-icon.png" alt="weather-icon" width="25" height="25" class="weather-icon"/>
+                    <p>5°</p>
+                    <p>1°</p>
+                </div>
+                <div class="day-weather">
+                    <p>Aujourd'hui</p>
+                    <p><img src="../assets/droplet.png" alt="weather-icon" class="weather-icon"/> 100%</p>
+                    <img src="../assets/rain-icon.png" alt="weather-icon" width="25" height="25" class="weather-icon"/>
+                    <img src="../assets/rain-icon.png" alt="weather-icon" width="25" height="25" class="weather-icon"/>
+                    <p>5°</p>
+                    <p>1°</p>
+                </div>
+                <div class="day-weather">
+                    <p>Aujourd'hui</p>
+                    <p><img src="../assets/droplet.png" alt="weather-icon" class="weather-icon"/> 100%</p>
+                    <img src="../assets/rain-icon.png" alt="weather-icon" width="25" height="25" class="weather-icon"/>
+                    <img src="../assets/rain-icon.png" alt="weather-icon" width="25" height="25" class="weather-icon"/>
+                    <p>5°</p>
+                    <p>1°</p>
+                </div>
+        </div>
+        <!-- card lever et coucher de soleil -->
+        <div class="card">
+            <div class="sunrise-card">
+                <p class="title-card"><b>Lever de soleil</b></p>
+                <p>7:10</p>
+                <img src="../assets/sunlight-img.png" alt="weather-icon" width="150" height="" class="sunset-sunlight"/>
+            </div>
+            <div class="sunset-card">
+                <p class="title-card"><b>Coucher de soleil</b></p>
+                <p>20:31</p>
+                <img src="../assets/sunset-img.png" alt="weather-icon" width="150" height="" class="sunset-sunlight"/>
+            </div>
+        </div>
+        <!--card uv vent humidité -->
+        <div class="card">
+            <div class="info-meteo">
+                <img src="../assets/sun.png" alt="weather-icon" width="50" height="" class="weather-icon"/>
+                <p><b>Indice uv</b></p>
+                <p>Faible</p>
+            </div>
+            <div class="info-meteo">
+                <img src="../assets/wind.png" alt="weather-icon" width="50" height="" class="weather-icon"/>
+                <p><b>Vent</b></p>
+                <p>21km/h</p>
+            </div>
+            <div class="info-meteo">
+                <img src="../assets/droplet-img.png" alt="weather-icon" width="40" height="" class="weather-icon"/>
+                <p><b>Humidité</b></p>
+                <p>87%</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'HomeView',
+}
+</script>
+
+<style scoped>
+.home {
+    background-color: #0F284C;
+    width: 100%;
+    height: 100%;
+}
+
+.card-dashboard {
+    display: flex;
+    flex-direction: row;
+
+}
+.card {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    background-color: #0F2D57;
+    border-radius: 20px;
+    margin:2em;
+    color:#CAF7FF;
+
+}
+.card-row {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    background-color: #0F2D57;
+    border-radius: 20px;
+    padding:0.5em;
+    margin:2em;
+    color:#CAF7FF
+}
+.left-card-dashboard{
+    padding:2em;
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    width:50%;
+    border:1px;
+    color:#CAF7FF;
+    margin-left:2em;
+}
+.right-card-dashboard{
+    padding:2em;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width:50%;
+    color:#CAF7FF
+}
+.hours-meteo {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin:1em;
+    padding:0.5em;
+}
+
+.info-meteo{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding:1em;
+    border-radius: 10px;
+    padding-top:1.5em;
+}
+.day-weather {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    padding:0.5em;
+}
+.meteo-day {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+}
+.celsius {
+    font-size: 5em;
+}
+.city{
+    font-size: 3em;
+}
+.weather-icon{
+    margin:1em;
+}
+.menu {
+    display: flex;
+    justify-content: end;
+    margin-right:2em;
+    margin-top:2em;
+}
+.title-card {
+    margin-top: 1.5em;
+}
+</style>
