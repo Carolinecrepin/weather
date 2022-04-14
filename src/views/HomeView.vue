c55c2447ae04bfad225362c517bf1ac00d5bd48b
caroline <crepin.caroline@gmail.com>
2022-04-14 10:28:23 +0200

    picture en icon weather

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,64 +7,85 @@
         <!--card dashboard meteo-->
         <div class="card-dashboard">
             <div class="left-card-dashboard">
-                <h2 class="celsius">5°</h2>
-                <h4 class="city">Arras</h4>
-                <p>1°/6° Ressenti 3°</p>
+                <h2 class="celsius">{{currentWeather.currentTemp}}°</h2>
+                <h4 class="city">{{currentWeather.city}}</h4>
+                <p>{{forecastingWeatherCity.tempMinDaily}}°/{{forecastingWeatherCity.tempMaxDaily}}° Ressenti {{currentWeather.feelTemp}}°</p>
                 <p>Vendredi, 11:41</p>
             </div>
             <div class="right-card-dashboard">
-                <img src="../assets/rain-clouds.png" alt="weather-img" class="weather-img"/>
+                <!--affichage image en fonction du rendu data des icones-->
+                <img src="../assets/sun_img.png" alt="sun" v-if="currentWeather.iconCurrentWeather === '01d'">
+                <img src="../assets/sun-cloud-img.png" alt="sun-cloud" v-else-if="currentWeather.iconCurrentWeather === '02d'">
+                <img src="../assets/light-clouds-img.png" alt="light-cloud" v-else-if="currentWeather.iconCurrentWeather === '03d'">
+                <img src="../assets/dark-clouds-img.png" alt="dark-cloud" v-else-if="currentWeather.iconCurrentWeather === '04d'">
+                <img src="../assets/rain-img.png" alt="rain" v-else-if="currentWeather.iconCurrentWeather === '09d'">
+                <img src="../assets/rain-sun-cloud-img.png" alt="rain-sun-cloud" v-else-if="currentWeather.iconCurrentWeather === '10d'">
+                <img src="../assets/storm-img.png" alt="storm" v-else-if="currentWeather.iconCurrentWeather === '11d'">
+                <img src="../assets/snow-img.png" alt="snow" v-else-if="currentWeather.iconCurrentWeather === '13d'">
+                <img src="../assets/frog-img.png" alt="frog" v-else-if="currentWeather.iconCurrentWeather === '50d'">
             </div>
         </div>
         <!--card meteo par heure-->
         <div class="card">
             <div class="hours-meteo">
+                <p><b>{{forecastingWeatherCity.hour}}</b></p>
+                <!-- day icon -->
+                <img src="../assets/sun_icon.png" alt="sun" v-if="forecastingWeatherCity.iconHour === '01d'">
+                <img src="../assets/sun-cloud-icon.png" alt="sun-cloud" v-else-if="forecastingWeatherCity.iconHour === '02d'">
+                <img src="../assets/cloudy_icon.png" alt="light-cloud" v-else-if="forecastingWeatherCity.iconHour === '03d'">
+                <img src="../assets/cloudy_icon.png" alt="dark-cloud" v-else-if="forecastingWeatherCity.iconHour === '04d'">
+                <img src="../assets/rain-icon.png" alt="rain" v-else-if="forecastingWeatherCity.iconHour === '09d'">
+                <img src="../assets/rain-sun-cloud_icon.png" alt="rain-sun-cloud" v-else-if="forecastingWeatherCity.iconHour === '10d'">
+                <img src="../assets/storm_icon.png" alt="storm" v-else-if="forecastingWeatherCity.iconHour === '11d'">
+                <img src="../assets/snow_icon.png" alt="snow" v-else-if="forecastingWeatherCity.iconHour === '13d'">
+                <img src="../assets/frog_icon.png" alt="frog" v-else-if="forecastingWeatherCity.iconHour === '50d'">
+                <!--night icon -->
+                <img src="../assets/sun_icon.png" alt="sun" v-else-if="forecastingWeatherCity.iconHour === '01n'">
+                <img src="../assets/sun-cloud-icon.png" alt="sun-cloud" v-else-if="forecastingWeatherCity.iconHour === '02n'">
+                <img src="../assets/cloudy_icon.png" alt="light-cloud" v-else-if="forecastingWeatherCity.iconHour === '03n'">
+                <img src="../assets/cloudy_icon.png" alt="dark-cloud" v-else-if="forecastingWeatherCity.iconHour === '04n'">
+                <img src="../assets/rain-icon.png" alt="rain" v-else-if="forecastingWeatherCity.iconHour === '09n'">
+                <img src="../assets/rain-sun-cloud_icon.png" alt="rain-sun-cloud" v-else-if="forecastingWeatherCity.iconHour === '10n'">
+                <img src="../assets/storm_icon.png" alt="storm" v-else-if="forecastingWeatherCity.iconHour === '11n'">
+                <img src="../assets/snow_icon.png" alt="snow" v-else-if="forecastingWeatherCity.iconHour === '13n'">
+                <img src="../assets/frog_icon.png" alt="frog" v-else-if="forecastingWeatherCity.iconHour === '50n'">
+                <p>{{forecastingWeatherCity.hourTemp}}°</p>
+                <p><img src="../assets/droplet.png" alt="weather-icon" class="weather-icon"/> {{forecastingWeatherCity.hourHumidity}}%</p>
+            </div>
+             <div class="hours-meteo">
                 <p><b>12:00</b></p>
                 <img src="../assets/rain-icon.png" alt="weather-icon" class="weather-icon"/>
                 <p>5°</p>
-                <li>_</li>
                 <p><img src="../assets/droplet.png" alt="weather-icon" class="weather-icon"/> 92%</p>
             </div>
              <div class="hours-meteo">
                 <p><b>12:00</b></p>
                 <img src="../assets/rain-icon.png" alt="weather-icon" class="weather-icon"/>
                 <p>5°</p>
-                <li>_</li>
                 <p><img src="../assets/droplet.png" alt="weather-icon" class="weather-icon"/> 92%</p>
             </div>
              <div class="hours-meteo">
                 <p><b>12:00</b></p>
                 <img src="../assets/rain-icon.png" alt="weather-icon" class="weather-icon"/>
                 <p>5°</p>
-                <li>_</li>
                 <p><img src="../assets/droplet.png" alt="weather-icon" class="weather-icon"/> 92%</p>
             </div>
              <div class="hours-meteo">
                 <p><b>12:00</b></p>
                 <img src="../assets/rain-icon.png" alt="weather-icon" class="weather-icon"/>
                 <p>5°</p>
-                <li>_</li>
                 <p><img src="../assets/droplet.png" alt="weather-icon" class="weather-icon"/> 92%</p>
             </div>
              <div class="hours-meteo">
                 <p><b>12:00</b></p>
                 <img src="../assets/rain-icon.png" alt="weather-icon" class="weather-icon"/>
                 <p>5°</p>
-                <li>_</li>
                 <p><img src="../assets/droplet.png" alt="weather-icon" class="weather-icon"/> 92%</p>
             </div>
              <div class="hours-meteo">
                 <p><b>12:00</b></p>
                 <img src="../assets/rain-icon.png" alt="weather-icon" class="weather-icon"/>
                 <p>5°</p>
-                <li>_</li>
-                <p><img src="../assets/droplet.png" alt="weather-icon" class="weather-icon"/> 92%</p>
-            </div>
-             <div class="hours-meteo">
-                <p><b>12:00</b></p>
-                <img src="../assets/rain-icon.png" alt="weather-icon" class="weather-icon"/>
-                <p>5°</p>
-                <li>_</li>
                 <p><img src="../assets/droplet.png" alt="weather-icon" class="weather-icon"/> 92%</p>
             </div>
         </div>
@@ -72,11 +93,11 @@
         <div class="card-row">
                 <div class="day-weather">
                     <p>Aujourd'hui</p>
-                    <p><img src="../assets/droplet.png" alt="weather-icon" class="weather-icon"/> 100%</p>
+                    <p><img src="../assets/droplet.png" alt="weather-icon" class="weather-icon"/> {{forecastingWeatherCity.humidityDaily}} %</p>
                     <img src="../assets/rain-icon.png" alt="weather-icon" width="25" height="25" class="weather-icon"/>
                     <img src="../assets/rain-icon.png" alt="weather-icon" width="25" height="25" class="weather-icon"/>
-                    <p>5°</p>
-                    <p>1°</p>
+                    <p>{{forecastingWeatherCity.tempMinDaily}} °</p>
+                    <p>{{forecastingWeatherCity.tempMaxDaily}} °</p>
                 </div>
                 <div class="day-weather">
                     <p>Aujourd'hui</p>
@@ -131,12 +152,12 @@
         <div class="card">
             <div class="sunrise-card">
                 <p class="title-card"><b>Lever de soleil</b></p>
-                <p>7:10</p>
+                <p>{{forecastingWeatherCity.currentSunrise}}</p>
                 <img src="../assets/sunlight-img.png" alt="weather-icon" width="150" height="" class="sunset-sunlight"/>
             </div>
             <div class="sunset-card">
                 <p class="title-card"><b>Coucher de soleil</b></p>
-                <p>20:31</p>
+                <p>{{forecastingWeatherCity.currentSunset}}</p>
                 <img src="../assets/sunset-img.png" alt="weather-icon" width="150" height="" class="sunset-sunlight"/>
             </div>
         </div>
@@ -144,18 +165,18 @@
         <div class="card">
             <div class="info-meteo">
                 <img src="../assets/sun.png" alt="weather-icon" width="50" height="" class="weather-icon"/>
-                <p><b>Indice uv</b></p>
-                <p>Faible</p>
+                <p><b>Indice UV</b></p>
+                <p>{{forecastingWeatherCity.currentUv}}</p>
             </div>
             <div class="info-meteo">
                 <img src="../assets/wind.png" alt="weather-icon" width="50" height="" class="weather-icon"/>
                 <p><b>Vent</b></p>
-                <p>21km/h</p>
+                <p>{{forecastingWeatherCity.currentWind}}</p>
             </div>
             <div class="info-meteo">
                 <img src="../assets/droplet-img.png" alt="weather-icon" width="40" height="" class="weather-icon"/>
                 <p><b>Humidité</b></p>
-                <p>87%</p>
+                <p>{{forecastingWeatherCity.humidityDaily}}%</p>
             </div>
         </div>
     </div>
@@ -166,16 +187,15 @@ export default {
     name:'HomeView',
     data() {
         return {
-                currentWeatherCity: null,
-                forecastingWeatherCity: null,
-                currentWeatherData:{},
-                forecastingData:{},
+                currentWeather: {
+                    iconCurrentWeather:""
+                },
+                forecastingWeatherCity: {},
         }
     },
     //Api qui permet de récupérer la position actuelle en utilisant la latitude et la longitude ce qui donne la position.coords
     async mounted() {
         await this.getCurrentPosition();
-
     },
     methods: {
         //methode pour récupérer la currentPosition en fonction de la latitude et la longitude
@@ -189,53 +209,46 @@ export default {
         //methode pour obtenir la meteo actuelle de la ville courante (header meteo)
         async getCurrentWeatherForCurrentCity(coordinates){
             const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.latitude}&lon=${coordinates.longitude}&units=metric&dt=1586468027&appid=5bdc0b308ad30be2f18120d0db9cbfca`);
-            const currentWeatherCity = await response.json();
-            const currentWeatherData = {
-                city: currentWeatherCity.name,
-                currentTemp: currentWeatherCity.main.temp,
-                minTemp:currentWeatherCity.main.temp_min,
-                maxTemp:currentWeatherCity.main.temp_max,
-                feelTemp:currentWeatherCity.main.feels_like,
-                iconCurrentWeather:currentWeatherCity.weather[0].icon,
+            const currentWeather = await response.json();
+            const currentWeatherDatas = {
+                city: currentWeather.name,          //nom de la ville courante
+                 //var date = new date(UNIX_Timestamp * 1000);
+                currentTemp: currentWeather.main.temp,  //température actuelle
+                minTemp:currentWeather.main.temp_min,   //température minimale
+                maxTemp:currentWeather.main.temp_max,   //température maximale
+                feelTemp:currentWeather.main.feels_like,    //température ressentie
+                iconCurrentWeather:currentWeather.weather[0].icon,  //image de la météo actuelle
             }
-            console.log(currentWeatherData)
-            return currentWeatherData;
+             return this.currentWeather = currentWeatherDatas;
+            //console.log(currentWeatherDatas)
+
         },
         //methode pour récupérer les prévisions météo de la ville en question (previsions)
         async getForecastingForCurrentCity(coordinates){
             const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.latitude}&lon=${coordinates.longitude}&units=metric&dt=1586468027&appid=5bdc0b308ad30be2f18120d0db9cbfca`);
             const forecastingWeatherCity = await response.json();
-            const forecastingData = {
+            const forecastingDatas = {
                 //données méteo actuelles
-                currentHour: forecastingWeatherCity.current.dt,
-                currentSunrise: forecastingWeatherCity.current.sunrise,
-                currentSunset: forecastingWeatherCity.current.sunset,
-                currentUv: forecastingWeatherCity.current.uvi,
-                currentWind: forecastingWeatherCity.current.wind_speed,
+                currentHour: forecastingWeatherCity.current.dt,         //heure actuelle     
+                currentSunrise: forecastingWeatherCity.current.sunrise,     //lever du soleil du jour
+                currentSunset: forecastingWeatherCity.current.sunset,       //coucher du soleil du jour
+                currentUv: forecastingWeatherCity.current.uvi,              //indice uv actuel
+                currentWind: forecastingWeatherCity.current.wind_speed,     //vitesse du vent actuelle
+                currentIcon: forecastingWeatherCity.current.weather[0].icon,   //icone météo actuelle
                 //données météo par heure
-                hour: forecastingWeatherCity.hourly[0].dt,
-                hourTemp: forecastingWeatherCity.hourly[0].temp,
-                hourHumidity: forecastingWeatherCity.hourly[0].humidity,
-                //données météo par jour (actuel)
-                hourDaily: forecastingWeatherCity.daily[0].dt,
-                tempMinDaily: forecastingWeatherCity.daily[0].temp.min,
-                tempMaxDaily: forecastingWeatherCity.daily[0].temp.max,
-                humidityDaily: forecastingWeatherCity.daily[0].humidity,
-                //données météo jour [1]
-                hourDaily1: forecastingWeatherCity.daily[1].dt,
-                tempMinDaily1: forecastingWeatherCity.daily[1].temp.min,
-                tempMaxDaily1: forecastingWeatherCity.daily[1].temp.max,
-                humidityDaily1: forecastingWeatherCity.daily[1].humidity,
-                //données météo jour [2]
-                hourDaily2: forecastingWeatherCity.daily[2].dt,
-                tempMinDaily2: forecastingWeatherCity.daily[2].temp.min,
-                tempMaxDaily2: forecastingWeatherCity.daily[2].temp.max,
-                humidityDaily2: forecastingWeatherCity.daily[2].humidity,
-
-                
+                hour: forecastingWeatherCity.hourly[0].dt,                  //heure
+                hourTemp: forecastingWeatherCity.hourly[0].temp,            //température de l'heure
+                hourHumidity: forecastingWeatherCity.hourly[0].humidity,    //humidité de l'heure
+                iconHour: forecastingWeatherCity.hourly[13].weather[0].icon,
+                //données météo par jour 
+                hourDaily: forecastingWeatherCity.daily[0].dt,              //heure du jour
+                tempMinDaily: forecastingWeatherCity.daily[0].temp.min,     //température minimale du jour
+                tempMaxDaily: forecastingWeatherCity.daily[0].temp.max,     //température max du jour
+                humidityDaily: forecastingWeatherCity.daily[0].humidity,    //taux d'humidité du jour 
+                iconDaily:forecastingWeatherCity.daily[0].weather[0].icon,     //icone du jour
             }
-            console.log(forecastingData)
-            return forecastingData;
+            //console.log(forecastingDatas)
+            return this.forecastingWeatherCity = forecastingDatas;
         }
             
     },
@@ -258,17 +271,18 @@ export default {
     display: flex;
     flex-direction: row;
     justify-content: space-evenly;
-    background-color: #0F2D57;
+    background: linear-gradient(#0F2D57, #224a82);
     border-radius: 20px;
     margin:2em;
     color:#CAF7FF;
 
 }
+
 .card-row {
     display: flex;
     flex-direction: column;
     justify-content: space-evenly;
-    background-color: #0F2D57;
+    background: linear-gradient(#0F2D57, #224a82);
     border-radius: 20px;
     padding:0.5em;
     margin:2em;
