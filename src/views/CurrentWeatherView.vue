22af226f8e442bcfceee91b0f3e707838afde256
caroline <crepin.caroline@gmail.com>
2022-04-15 15:14:32 +0200

    factory et repository

--- src/views/CurrentWeatherView.vue
+++ src/views/CurrentWeatherView.vue
@@ -157,6 +157,8 @@
 <script>
 import CurrentWeatherIcon from "@/components/CurrentWeatherIcon.vue"
 import HoursWeatherIcon from "@/components/HoursWeatherIcon.vue"
+import weatherRepository from "@/repository/weather.repository"
+import weatherFactory from "@/factory/weather.factory"
 export default {
     name:'HomeView',
     components: {
@@ -178,62 +180,14 @@ export default {
             async getCurrentPosition(){
             navigator.geolocation.getCurrentPosition(async position => {
                 const coordinates = position.coords;
-                await this.getCurrentWeather(coordinates);
-                await this.getForecastWeather(coordinates)
+                //factory de la methode setForecastWeather qui fabrique un nouvel objet a partir de de la method getforecastWeather 
+                const forecastWeather = await weatherRepository.getForecastWeather(coordinates);
+                 this.forecastWeather = weatherFactory.setForecastWeather(forecastWeather);
+                //factory de la methode setCurrentWeather qui fabrique un nouvel objet a partir de de la method getCurrentWeather
+                const currentWeather = await weatherRepository.getCurrentWeather(coordinates);
+                this.currentWeather = weatherFactory.setCurrentWeather(currentWeather);
             })
-        },
-        //methode pour obtenir la meteo actuelle de la ville courante (header meteo)
-        async getCurrentWeather(coordinates){
-            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.latitude}&lon=${coordinates.longitude}&units=metric&dt=1586468027&appid=5bdc0b308ad30be2f18120d0db9cbfca`);
-            const currentWeather = await response.json();
-            const currentWeatherDatas = {
-                city: currentWeather.name,          //nom de la ville courante
-                currentTemp: currentWeather.main.temp,  //température actuelle
-                minTemp:currentWeather.main.temp_min,   //température minimale
-                maxTemp:currentWeather.main.temp_max,   //température maximale
-                feelTemp:currentWeather.main.feels_like,    //température ressentie
-                iconCurrentWeather:currentWeather.weather[0].icon,  //image de la météo actuelle
-            }
-             return this.currentWeather = currentWeatherDatas;
-            //console.log(currentWeatherDatas)
-
-        },
-        //methode pour récupérer les prévisions météo de la ville en question (previsions)
-        async getForecastWeather(coordinates){
-            const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.latitude}&lon=${coordinates.longitude}&units=metric&dt=1586468027&appid=5bdc0b308ad30be2f18120d0db9cbfca`);
-            const forecastWeather = await response.json();
-            const forecastingDatas = {
-                //données méteo actuelles
-                currentHour: forecastWeather.current.dt,         //heure actuelle     
-                currentSunrise: forecastWeather.current.sunrise,     //lever du soleil du jour
-                currentSunset: forecastWeather.current.sunset,       //coucher du soleil du jour
-                currentUv: forecastWeather.current.uvi,              //indice uv actuel
-                currentWind: forecastWeather.current.wind_speed,     //vitesse du vent actuelle
-                currentIcon: forecastWeather.current.weather[0].icon,   //icone météo actuelle
-                //données météo par heure
-                hour: forecastWeather.hourly[0].dt,                  //heure
-                hourTemp: forecastWeather.hourly[0].temp,            //température de l'heure
-                hourHumidity: forecastWeather.hourly[0].humidity,    //humidité de l'heure
-                iconHour: forecastWeather.hourly[13].weather[0].icon,
-                //données météo par jour 
-                hourDaily: forecastWeather.daily[0].dt,              //heure du jour
-                tempMinDaily: forecastWeather.daily[0].temp.min,     //température minimale du jour
-                tempMaxDaily: forecastWeather.daily[0].temp.max,     //température max du jour
-                humidityDaily: forecastWeather.daily[0].humidity,    //taux d'humidité du jour 
-                iconDaily:forecastWeather.daily[0].weather[0].icon,     //icone du jour
-            }
-            //console.log(forecastingDatas)
-            return this.forecastWeather = forecastingDatas;
-        },
-
-        async setCurrentWeather(forecastWeather){
-            return 
-        },
-
-        async setForecastWeather(currentWeather){
-            return
-        }
-            
+        },     
     },
 }
 </script>
